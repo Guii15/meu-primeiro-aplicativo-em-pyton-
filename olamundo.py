bf19591c7f29d991677657125705e8aa66490234
Guii15 <116568504+Guii15@users.noreply.github.com>
2022-10-25 11:18:39 -0300

    criei aplicativo
    criei o aplicativo sobre meus dados

---
+++ olamundo.py
@@ -0,0 +1,12 @@
+# coletando dados do usuario
+nome = input("digete seu nome: ")
+idade = int (input("digete sua idade: "))
+altura = float (input("digete sua altura: "))
+peso = float (input("digete seu peso: "))
+
+print("-" * 30)
+print("os dados colatados foram: ")
+print("seu nome: ",nome)
+print("idade: ",idade," anos")
+print("altura: ",altura)
+print("peso: ",peso, "kgs")
